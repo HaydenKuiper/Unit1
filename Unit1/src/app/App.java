106c77ec8145730d082b8935b8157c860217e54c
HaydenKuiper <820884@mystma.org>
2020-04-08 17:44:24 +0000

    Added top five movies

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,15 @@
+package app;
+
+import sun.awt.AWTAccessor.SystemColorAccessor;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("1. Polar Express");
+        System.out.println("2. lord of the rings triology");
+        System.out.println("3. Dead Pool");
+        System.out.println("4. the MCU");
+        System.out.println("5. Stand By Me");
+
+    
+    
+}
