2b325af23cd2642da60f56402d1475e9bedf536c
HaydenKuiper <820884@mystma.org>
2020-04-14 19:32:02 +0000

    fun time code

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,23 +1,31 @@
 package app;
 public class App {
     public static void main(String[] args) throws Exception {
-        String Person1 = "Hayden";
-        String Verb1 = "code";
-        String Adjective1 = "eggs";
-        String Verb2 = "bacon";
-        String Place1 = "home";
-        String Person2= "John";
-        String Adjective2 = "Super";
-        String Place2 = "market";
-        String Verb3 = "chill";
+        int count = 0;
+        String color = "red";
 
-        System.out.println(Person1 + " " + "is a programer who likes to" + " " + Verb1);
-        System.out.println("every day of the week they make" + " " + Adjective1 + " " + "for breakfest and" + " " + Verb2 + " " + "at" + " " + Place1 + " " + "with" + " " + Person2);
-        System.out.println("somethimes, they go to the" + " " + Adjective2 + " " + Place2 + " " + "with" + " " + Person2 + " " + "so that they can" + " " + Verb3 + " " + "togehter");
-    
+
+        while (count <= 10) {
+            System.out.println("the count  is " + count);
+            count = count + 1;
+         }
+         System.out.println("the count is finished");
+
+         while (color == "red") {
+             System.out.println("the color is " + color);
+             color = "green";
+         }
+        System.out.println("color is finished");
+       
+        boolean livesInMinnesota = true;
         
-        
-        
+        while (livesInMinnesota == true) {
+            System.out.println("i dont live in minnesota");
+            System.out.println(livesInMinnesota);
+            livesInMinnesota = false;
+         }
+        System.out.println("livesInMinnesota = false");
+
 
     
     }
