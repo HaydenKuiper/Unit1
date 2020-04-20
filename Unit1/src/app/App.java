a4800472c6fbe2bfdb97731255fa1ad0be014fb7
HaydenKuiper <820884@mystma.org>
2020-04-20 19:53:46 +0000

    if statements

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,26 +1,16 @@
 package app;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
-        for (int i = 22; i <= 28; i = i +3) {
-            System.out.println("the count is " + i);
-            System.out.println("lets keep counting");
-        }
-        for (int i = 1; i <= 9; i = i + 1 ) {
-            System.out.println(i *i);
-            System.out.println(i + 3);
-            System.out.println(i);
-        }
-        for (int i = 3; -81 <= i; i = -i * i) {
-            System.out.println(i);
-        }
-        for (int i = 50; i >= 5; i = i -5) {
-            System.out.println(i);
-        }
-        for (int i = 2; i <= 32; i = i * 2) {
-            System.out.println(i);
-        }
-
+        for (int i = 1; i <= 20; i++) {
+             if ( i % 4 == 0) {
+            System.out.println("the number is divisable by 4");
+        }else {
+            System.out.println("the number is not divisable by 4");
+        }   
+        }   
+       
 
     
     }
