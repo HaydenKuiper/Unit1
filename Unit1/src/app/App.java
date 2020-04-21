76722d92d8597c9f981ce3ccc13baaac36626b7c
HaydenKuiper <820884@mystma.org>
2020-04-21 18:27:22 +0000

    loops challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,16 +2,29 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        for (int i = 1; i <= 20; i++) {
-             if ( i % 4 == 0) {
-            System.out.println("the number is divisable by 4");
-        }else {
-            System.out.println("the number is not divisable by 4");
-        }   
-        }   
-       
+          
+       for (int i = 0; i < 20; i++) {
+            if (i == 10) {
+                int x = 1;
+                 while (x <= 6) {
+                    System.out.println(i);
+                    x++;
+                }
+            }else {
+                System.out.println(i);
+            } 
+        }
+        System.out.println("End First Loop");
+        System.out.println("Start Second Loop");
 
-    
+        for (int i = -1; i < 20; i++ ) {
+            if (i %3 == 0){
+
+        }
+        else{
+            System.out.println(i);
+        }
     }
 }
+}
+
