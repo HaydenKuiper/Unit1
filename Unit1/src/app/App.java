a79941a5a9165b914f98d7791e357f9837d75e19
HaydenKuiper <820884@mystma.org>
2020-04-22 17:57:45 +0000

    logic Challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,26 +2,28 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-          
-       for (int i = 0; i < 20; i++) {
+        for (int i = 20; i >= 1; i--) {
             if (i == 10) {
-                int x = 1;
-                 while (x <= 6) {
+                int x = 1; 
+                while (x <= 6) {
                     System.out.println(i);
                     x++;
                 }
             }else {
                 System.out.println(i);
-            } 
+            }
         }
-        System.out.println("End First Loop");
-        System.out.println("Start Second Loop");
+        System.out.println("End First loop");
+        System.out.println("start Second Loop");
 
-        for (int i = -1; i < 20; i++ ) {
-            if (i %3 == 0){
 
+        for(int i = 0; 36 >= i; i = i + 4) {
+            if (i%6 == 0) {
+                System.out.println(i);
+                System.out.println("Hey!");
+                
         }
-        else{
+        else{ 
             System.out.println(i);
         }
     }
