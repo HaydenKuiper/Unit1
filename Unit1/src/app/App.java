f1e275812cc9cac3a109c65aaeee12c7a4158113
HaydenKuiper <820884@mystma.org>
2020-04-24 16:58:00 +0000

    double or single digit

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,32 +2,30 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("start first loop");
-        for (int i = 20; i >= 1; i--) {
-            if (i == 10) {
-                int x = 1; 
-                while (x <= 6) {
-                    System.out.println(i);
-                    x++;
-                }
-            }else {
-                System.out.println(i);
-            }
+    
+    System.out.println("Output :");
+    System.out.println("Double or single digit");
+    for( int i = 20; i > 0; i = i - 1) {
+        if (i > 9){
+            System.out.println( i + " " + "is double digit");
         }
-        System.out.println("End First loop");
-        System.out.println("start Second Loop");
-
-
-        for(int i = 0; 36 >= i; i = i + 4) {
-            if (i%6 == 0) {
-                System.out.println(i);
-                System.out.println("Hey!");
-                
-        }
-        else{ 
-            System.out.println(i);
-        }
+        else {
+            System.out.println( i + " " + "is single digit");
+        } 
     }
+    System.out.println("Nice!");
+    System.out.println("fast food");
+    {for ( int i = 1; i < 6; i = i++)
+    if ( i == 100);
+    else {
+        System.out.println("burgers");
+        System.out.println("fries");
+        System.out.println("nice");
+    
+    } 
+    }
+    
+    
 }
 }
 
