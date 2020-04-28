8194ee5fc051faee8bdd55fb81f13b2f283ab100
HaydenKuiper <820884@mystma.org>
2020-04-28 16:20:58 +0000

    user input

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,29 +1,36 @@
 package app;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("please enter your first name");
+        String firstname = input.nextLine();
+        System.out.println("please enter your last name");
+        String lastname = input.nextLine();
+        
+       
+
+        System.out.println("please enter your age");
+        String age = input.nextLine();
+        System.out.println("please enter grade");
+        String grade = input.nextLine();
+        System.out.println("please enter your favorite color");
+        String favcolor = input.nextLine();
+        System.out.println("hello," + " " + firstname);
+        System.out.println("hello, " + lastname);
+        System.out.println("you are this old " + age);
+        System.out.println("you are in this grade " + grade);
+        System.out.println("this is your favorite color " + favcolor);
+        
+
+
     
-  System.out.println("start first loop");
-  for (int i = 0; i <= 10; i = i + 1) {
-      System.out.println(i);
-      if (i == 5) {
-          System.out.println("five");
-      }
-  }
-  System.out.println("end first loop");
-  System.out.println("start second loop");
-  for (int i = 100; i < 151; i = i + 10) {
-      System.out.println(i);
-  }
-  for ( int i = 150; i <= 200; i = i + 1) {
-      System.out.println(i);
-      if ( 160 < 200) {
-          System.out.println(  + 10);
-      }
-  }
   
     
     
-}
+    }
 }
 
