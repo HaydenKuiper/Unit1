bfa49123eccd8189c1ca15216d48dab8d5719cae
HaydenKuiper <820884@mystma.org>
2020-04-29 18:19:16 +0000

    scanner object

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,27 +6,17 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("please enter your first name");
-        String firstname = input.nextLine();
-        System.out.println("please enter your last name");
-        String lastname = input.nextLine();
+        System.out.println("please enter your password");
+            String password = input.nextLine();
+        {
+            if (password.equals("183726073"))
+            {System.out.println("Access Granted");
+            
+            } else 
+            {System.out.println("Access Denied"); }
+
+        }
         
-       
-
-        System.out.println("please enter your age");
-        String age = input.nextLine();
-        System.out.println("please enter grade");
-        String grade = input.nextLine();
-        System.out.println("please enter your favorite color");
-        String favcolor = input.nextLine();
-        System.out.println("hello," + " " + firstname);
-        System.out.println("hello, " + lastname);
-        System.out.println("you are this old " + age);
-        System.out.println("you are in this grade " + grade);
-        System.out.println("this is your favorite color " + favcolor);
-        
-
-
     
   
     
