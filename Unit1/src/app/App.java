6fe18d68481f46083e756f639379826e7cdb3a57
HaydenKuiper <820884@mystma.org>
2020-05-05 17:28:33 +0000

    day 24

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,12 +5,36 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("what is the count");
-        int count = input.nextInt();
-
-        for (int x = 1; x <= count; x++) {
-            System.out.println(x);
+        System.out.println("how many inches are in a foot");
+        String foot = input.nextLine();
+        if (foot.equals("12")) {
+            System.out.println("correct");
         }
+        else {
+            System.out.println("incorrect");
+        }
+        System.out.println("what is the capital of Minnesota?");
+        String capital = input.nextLine();
+        if (capital.equals("Minneapolis")) {
+            System.out.println("correct");
+        }
+        else {
+            System.out.println("incorrect");
+        }
+        System.out.println("what year is it");
+        String year = input.nextLine();
+        if (year.equals("2020")) {
+            System.out.println("correct");
+        }
+        else {
+            System.out.println("incorrect");
+        }
+
+        
+
+       
+        
+
         
     
   
