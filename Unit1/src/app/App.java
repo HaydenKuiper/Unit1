00a6463c7b2055bb56b0d0f4d6f6393ce17db641
HaydenKuiper <820884@mystma.org>
2020-05-12 14:20:41 +0000

    simple calculator

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,37 +1,24 @@
 package app;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("please enter a noun");
-       System.out.println("will is a" + " " + input.nextLine() + " " + "that looks for missing people");
-       System.out.println("please enter a name");
-       System.out.println("one of the people he is looking for is" + " " + input.nextLine() + " " +"who was last seen at the park");
-       System.out.println("please enter a verb");
-       System.out.println("when he arrived at the park he started to" + " " + input.nextLine() + " a statue then he noticed something a piece of a shirt the exact same one the missing person was wearing" );
-       System.out.println("please enter adjective");
-       System.out.println("then he started to notice a pattern there was a line of cloth leading to the zoo with a " + " " + input.nextLine() + " " + " object blocknig his way but the thing was is that object was not supposed to be there in the first place");
-       System.out.println("please eneter a place");
-       System.out.println("as he went around the mysteroius object he saw a shimmer in the air he then aproached it and went through and found himself in" + " " + input.nextLine() + " where nothing seemed real there where men in amrmour fighting weird faced looking people it almost look like a scene from the Lord Of The Rings");
-       System.out.println("please enter a food");
-       System.out.println("he looked around and started running away from the carnage he kept runnign until it was dark he then approached a house of some sort a house that was burried in a hillisde and had a door the size of a small child. He entered and found a little man who was almost expecting hime and said welcome back detective would you like some" + " " + input.nextLine() + " " + " he pollitley declined and askeed where he was");
-       System.out.println("please enter a animal");
-       System.out.println("the little man lauged a bit and said he was in Middle earth. The man was in disbeliefe and said there was no such thing only in the movies then out of the corner of his eyer he noticed someting a creature of some sort a " + " " + input.nextLine() + " " + "with scales of some sort. Do you believe now the little man said. All i could do was stare in shock" );
-       System.out.println("please enter a number");
-       System.out.println("I then repeated to my self this cant be real it shouldnt be real I just started pinching myself then the next thing im gone and I am lying down in my bed and it is" + " " + input.nextLine() + " " + " am and I am sweating bullets. next thing you know I see the creature then darkness");
 
+        System.out.println("please enter first number");
+        int firstnumber = input.nextInt();
         
+        System.out.println("please enter second number");
+        int secondnumber = input.nextInt();
 
-       
-        
+        System.out.println("press 1 to add, press 2 to subtract, press three to divide press 4 to multiplie");
+        input.nextLine();
+        if (.funtion.equals("1"));
 
-        
-    
-  
+
     
     
     }
 }
-
